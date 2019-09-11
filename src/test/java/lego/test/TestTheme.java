2b98ae296902c89f54135f6da375635a3cc4ee6e
iltafp <iltaf.pavy@gmail.com>
2019-09-11 12:05:08 +0200

    add J11 version and TestTheme

---
+++ src/test/java/lego/test/TestTheme.java
@@ -0,0 +1,14 @@
+package lego.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+public class TestTheme {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
